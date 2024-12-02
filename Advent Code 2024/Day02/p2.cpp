cde7b76e94a3babbb7e1d5820c319acecd361007
thezeeshanhassan <thezeeshanhassan@gmail.com>
2024-12-02 21:48:42 +0500

    Advent Code 02 Completed

---
+++ Advent Code 2024/Day02/p2.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+bool isSafeReport(const vector<int> &report)
+{
+    bool status{true};
+    int difference{};
+    bool assending{true};
+    if (report[0] > report[1])
+    {
+        assending = false;
+    }
+    for (int i{1}; i < report.size(); i += 1)
+    {
+        difference = abs(report[i] - report[i - 1]);
+        if (difference < 1 || difference > 3)
+        {
+            return false;
+        }
+    }
+
+    for (int i{1}; i < report.size(); i += 1)
+    {
+        if (assending == true)
+        {
+            if (report[i] < report[i - 1])
+            {
+                status = false;
+                break;
+            }
+        }
+        if (assending == false)
+        {
+            if (report[i] > report[i - 1])
+            {
+                status = false;
+                break;
+            }
+        }
+    }
+    return status;
+}
+
+bool isSafeAfterRemovingOneLevel(vector<int> report)
+{
+
+    for (int i{}; i < report.size(); i++)
+    {
+        report.erase(report.begin() + i);
+
+        if (isSafeReport(report))
+        {
+            return true;
+        }
+
+        report.insert(report.begin() + i, report[i]);
+    }
+    return false;
+}
+int main()
+{
+    ifstream inputFile("input.txt");
+    if (!inputFile)
+    {
+        cerr << "Error opening file!" << endl;
+        return 1;
+    }
+
+    string line;
+    int safe{};
+
+    while (getline(inputFile, line))
+    {
+
+        stringstream ss(line);
+        vector<int> report;
+        int num;
+        while (ss >> num)
+        {
+            report.push_back(num);
+        }
+
+        if (isSafeReport(report))
+        {
+            safe += 1;
+        }
+
+        else if (isSafeAfterRemovingOneLevel(report))
+        {
+            safe += 1;
+        }
+
+        report.clear();
+    }
+
+    inputFile.close();
+    cout << "Number of safe reports: " << safe << endl;
+    return 0;
+}
